45fc01d0c552efe3adfad3f61cf8531dc89417b2
akamikado <manishvasireddy1234@gmail.com>
2025-12-20 17:40:00 +0530

    added `clear` method to `db` module and fixed error messages

--- pkg/stdlib/db.go
+++ pkg/stdlib/db.go
@@ -194,23 +194,23 @@ var DbBuiltins = map[string]*object.Builtin{
 	"db.delete": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 2 {
-				return &object.Error{Code: "E7001", Message: "db.has() takes exactly 2 arguments"}
+				return &object.Error{Code: "E7001", Message: "db.delete() takes exactly 2 arguments"}
 			}
 
 			db, ok := args[0].(*object.Database)
 			if !ok {
-				return &object.Error{Code: "E7001", Message: "db.has() requires a Database object as first argument"}
+				return &object.Error{Code: "E7001", Message: "db.delete() requires a Database object as first argument"}
 			}
 
 			if db.IsClosed.Value {
 				return &object.ReturnValue{Values: []object.Object{
-					&object.Error{Code: "E17005", Message: "db.has() cannot operate on closed database"},
+					&object.Error{Code: "E17005", Message: "db.delete() cannot operate on closed database"},
 				}}
 			}
 
 			key, ok := args[1].(*object.String)
 			if !ok {
-				return &object.Error{Code: "E7001", Message: "db.has() requires a String as second argument"}
+				return &object.Error{Code: "E7001", Message: "db.delete() requires a String as second argument"}
 			}
 
 			deleted := db.Store.Delete(key)
@@ -277,7 +277,28 @@ var DbBuiltins = map[string]*object.Builtin{
 		},
 	},
 
-	"db.clear": {},
+	"db.clear": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return &object.Error{Code: "E7001", Message: "db.clear() takes exactly 1 argument"}
+			}
+
+			db, ok := args[0].(*object.Database)
+			if !ok {
+				return &object.Error{Code: "E7001", Message: "db.clear() requires a Database object as argument"}
+			}
+
+			if db.IsClosed.Value {
+				return &object.ReturnValue{Values: []object.Object{
+					&object.Error{Code: "E17005", Message: "db.clear() cannot operate on closed database"},
+				}}
+			}
+
+			db.Store = *object.NewMap()
+			
+			return &object.Nil{}
+		},
+	},
 
 	"db.save": {
 		Fn: func(args ...object.Object) object.Object {
